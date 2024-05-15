6cf77bb479e4b63d843fa8bb96dc5349baa9e81f
Rick(Rugui) Chen <rickruguichen@google.com>
2024-05-14 22:22:11 -0400

    Add ray training

--- ray-train/train.py
+++ ray-train/train.py
@@ -20,10 +20,10 @@ def get_dataloaders(batch_size):
     # Transform to normalize the input images
     transform = transforms.Compose([ToTensor(), Normalize((0.5,), (0.5,))])
 
-    with FileLock(os.path.expanduser('/data.lock')):
+    with FileLock(os.path.expanduser('~/data.lock')):
         # Download training data from open datasets
         training_data = datasets.FashionMNIST(
-            root='/data',
+            root='~/data',
             train=True,
             download=True,
             transform=transform,
@@ -31,7 +31,7 @@ def get_dataloaders(batch_size):
 
         # Download test data from open datasets
         test_data = datasets.FashionMNIST(
-            root='/data',
+            root='~/data',
             train=False,
             download=True,
             transform=transform,
@@ -120,7 +120,7 @@ def train_func_per_worker(config: Dict):
         # ===============================
         base_model = (model.module
             if isinstance(model, DistributedDataParallel) else model)
-        checkpoint_dir = '/checkpoint'#tempfile.mkdtemp()
+        checkpoint_dir = '~/checkpoint'#tempfile.mkdtemp()
         torch.save(
             {"model_state_dict": base_model.state_dict()},
             os.path.join(checkpoint_dir, "model.pt"),
