355807c500c595c4e515488131d9c2c40a023762
Rick(Rugui) Chen <rickruguichen@google.com>
2024-05-26 17:31:58 -0400

    workbench notebook

---
+++ workbench/setup.sh
@@ -0,0 +1,8 @@
+pip install -U "skypilot-nightly[kubernetes,gcp]"
+export PATH=$PATH:/home/user/.local/bin
+sudo apt-get update
+sudo apt-get install -y rsync
+sudo apt-get install -y socat
+sudo apt-get install -y netcat
+sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
+
